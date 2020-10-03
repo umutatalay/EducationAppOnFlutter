ea85706496a80885bc75107f70fc4d78f34d6b71
umutatalay <umutatalayumut@gmail.com>
2020-10-03 22:02:05 +0300

    Hope

---
+++ lib/WelcomeScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  static const String id='welcome_screen';
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('selam')),
+    );
+
+  }
+}
