c1beb3621468c1e7147a0e2d351b266fb9369367
umutatalay <umutatalayumut@gmail.com>
2020-10-04 02:59:49 +0300

    Hope 4.10.2020

--- lib/WelcomeScreen.dart
+++ lib/WelcomeScreen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/animation.dart';
+
+
 // Burada Logo Giriş ve Kayıt ol butonları olacak
 class WelcomeScreen extends StatefulWidget {
   static const String id='welcome_screen';
@@ -6,11 +10,98 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen>{
+class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin{
+  AnimationController controller;
+
+
+
+  @override
+  void initState() {
+
+    // TODO: implement initState
+    super.initState();
+    controller= AnimationController(
+      duration: Duration(seconds: 3),
+      vsync: this,
+    );
+    controller.forward();
+    controller.addListener(() {
+      setState(() {
+
+      });
+      print(controller.value);
+
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('selam')),
+      backgroundColor: Colors.lightBlueAccent,
+      body: Padding(
+        padding:EdgeInsets.symmetric(horizontal: 40.0),
+        child:Column(
+          children: <Widget>[
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+
+              children: <Widget>[
+                TypewriterAnimatedTextKit(
+                  text: ['coding ...','coding;','<coding>'],
+                  textStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize:22.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Codelike',
+                  ),
+
+                ),
+
+
+              ],
+            ),
+            SizedBox(
+              height: 60.0,
+            ),
+            Row(
+              children: <Widget>[
+                RaisedButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                      side: BorderSide(color: Colors.black)),
+                  onPressed: () {},
+                  color: Colors.lightBlueAccent,
+                  textColor: Colors.white,
+                  child: Text("Giriş Yap".toUpperCase(),
+                      style: TextStyle(fontSize: 22,fontWeight: FontWeight.w800),
+
+
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                RaisedButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                      side: BorderSide(color: Colors.black)),
+                  onPressed: () {},
+                  color: Colors.lightBlueAccent,
+                  textColor: Colors.white,
+                  child: Text("Kayıt Ol".toUpperCase(),
+                      style: TextStyle(fontSize: 22)),
+                ),
+              ],
+            ),
+
+
+          ],
+        )
+
+      )
     );
 
   }
