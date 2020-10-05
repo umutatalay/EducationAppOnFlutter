7f5b425b8c7e08650ec0384165e3121a0102db20
umutatalay <umutatalayumut@gmail.com>
2020-10-05 18:36:55 +0300

    Hope 5.10.20 00.59

--- lib/Categories.dart
+++ lib/Categories.dart
@@ -10,20 +10,34 @@ class _CategoriesState extends State<Categories> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white70,
       body: Column(
+
         children: <Widget>[
           Row(
             children: <Widget>[
               Text('Kategoriler'),
-              Container(
-                margin: EdgeInsets.all(20.0),
-                decoration: BoxDecoration(
-                  color: Colors.orange,
-                  borderRadius: BorderRadius.circular(15.0),
-                ),
-              )
             ],
           ),
+          Container(
+            height: 400.0,
+            width: 400.0,
+
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(12.0),
+                  topRight: Radius.circular(12.0),
+                  bottomLeft: Radius.circular(12.0),
+                  bottomRight: Radius.circular(12.0),
+                )
+            ),
+            child: CircleAvatar(
+              radius: 50,
+              backgroundImage: NetworkImage('https://pbs.twimg.com/profile_images/1248312733801406464/swAuOAfC_400x400.jpg'),
+            ),
+          ),
+
 
         ],
       ),
