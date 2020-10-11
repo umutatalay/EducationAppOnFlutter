78457d076aafad7983e5048123708fd987235ff5
umutatalay <umutatalayumut@gmail.com>
2020-10-12 02:46:46 +0300

    12.10

--- lib/Lessons.dart
+++ lib/Lessons.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 var text;
-
 class LessonScreen extends StatefulWidget {
   static const String id = 'lesson_screen';
 
@@ -19,8 +18,11 @@ class _LessonScreenState extends State<LessonScreen> {
     );
   }
 }
-
+var liste = new List();
+// ignore: must_be_immutable
 class UserInformation extends StatelessWidget {
+
+  String veriler='';
   @override
   Widget build(BuildContext context) {
     CollectionReference users =
@@ -38,50 +40,41 @@ class UserInformation extends StatelessWidget {
         }
         ListView(
           children: snapshot.data.docs.map((DocumentSnapshot document) {
-            text = document.data()['Content'].toString();
+            liste.add(document.id);
           }).toList(),
+
         );
+       NewWidget();
+
 
         return Scaffold(
-          body: Container(
-            height: 200.0,
-            width: 400.0,
-            decoration: BoxDecoration(
-              color: Colors.pinkAccent,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(12.0),
-                topRight: Radius.circular(12.0),
-                bottomLeft: Radius.circular(12.0),
-                bottomRight: Radius.circular(12.0),
-              ),
-            ),
-            child: Center(
-              child: Wrap(
-                spacing: 8.0, // gap between adjacent chips
-                runSpacing: 4.0, // gap between lines
-                direction: Axis.horizontal,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        text,
-                        style: TextStyle(
-                          fontFamily: 'KursAdi',
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20.0,
-                          color: Colors.white,
-                        ),),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
+          body: NewWidget(),
         );
       },
     );
+  }
+}
+
+class NewWidget extends StatelessWidget {
+
+  @override
+
+  Widget build(BuildContext context) {
+
+      print('sa');
+      return Scaffold(
+        body:
+          Center(
+            child: Container(
+              child: Column(
+                children: [for(var b in liste)Text(b.toString())],
+              ),
+            ),
+          )
+
+      );
+
+
   }
 }
 
