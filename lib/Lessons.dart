8c00ea9d79242d5f36c612e421f1f6be96cd653c
Yunus Emre <ycelik.mdbf17@iste.edu.tr>
2020-10-11 20:34:21 +0200

    YAŞADIK ABİJİMMMMM

--- lib/Lessons.dart
+++ lib/Lessons.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
+var text;
+
 class LessonScreen extends StatefulWidget {
   static const String id = 'lesson_screen';
 
@@ -12,7 +14,7 @@ class _LessonScreenState extends State<LessonScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.pink,
+      backgroundColor: Colors.white70,
       body: Center(child: UserInformation()),
     );
   }
@@ -22,7 +24,7 @@ class UserInformation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     CollectionReference users =
-        FirebaseFirestore.instance.collection('kodekspres');
+    FirebaseFirestore.instance.collection('HTML');
 
     return StreamBuilder<QuerySnapshot>(
       stream: users.snapshots(),
@@ -34,15 +36,52 @@ class UserInformation extends StatelessWidget {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return Text("Loading");
         }
-        return ListView(
+        ListView(
           children: snapshot.data.docs.map((DocumentSnapshot document) {
-            print(document.data().length.toString());
-            return ListTile(
-              title: Text(document.data()['Content'].toString()),
-            );
+            text = document.data()['Content'].toString();
           }).toList(),
+        );
+
+        return Scaffold(
+          body: Container(
+            height: 200.0,
+            width: 400.0,
+            decoration: BoxDecoration(
+              color: Colors.pinkAccent,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(12.0),
+                topRight: Radius.circular(12.0),
+                bottomLeft: Radius.circular(12.0),
+                bottomRight: Radius.circular(12.0),
+              ),
+            ),
+            child: Center(
+              child: Wrap(
+                spacing: 8.0, // gap between adjacent chips
+                runSpacing: 4.0, // gap between lines
+                direction: Axis.horizontal,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        text,
+                        style: TextStyle(
+                          fontFamily: 'KursAdi',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                          color: Colors.white,
+                        ),),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
         );
       },
     );
   }
 }
+
